9f738009cc11084cd4de315df80b4bde39a09227
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2023-02-25 15:02:02 -0500

    add console tracing

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,6 +21,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
@@ -31,6 +32,11 @@ backend = os.getenv('BACKEND_URL')
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# Show this in the logs within the backend-flask app (STDOUT)
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
