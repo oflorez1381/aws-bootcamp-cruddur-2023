1700b595e1512ed5a2a2bae16ab6d38368248abc
oflorez1381 <oscarflorez1381@gmail.com>
2023-02-25 15:54:09 -0500

    Week 02 live class (#7)
    * instrument honeycomb
    
    * add additional ports
    
    * add console tracing
    
    * add spans and attributes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,9 +14,38 @@ from services.create_message import *
 from services.show_activity import *
 from services.notifications_activities import *
 
+# HoneyComb ---------
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
+
+# HoneyComb ---------
+## Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+
+# Show this in the logs within the backend-flask app (STDOUT)
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
+
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+## Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+# HoneyComb ---------
+
 origins = [frontend, backend]
 cors = CORS(
   app, 
