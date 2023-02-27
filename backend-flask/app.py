6b143c9d9555d27f67d8d59456c807da66778c90
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2023-02-27 11:50:55 -0500

    added clouwatch support

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -27,6 +27,11 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# CloudWatch Logs ----
+import watchtower
+import logging
+from time import strftime
+
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -54,6 +59,16 @@ RequestsInstrumentor().instrument()
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
+
+# Cloudwatch ------
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
 
 origins = [frontend, backend]
 cors = CORS(
@@ -101,7 +116,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
@@ -156,6 +171,14 @@ def data_activities_reply(activity_uuid):
   else:
     return model['data'], 200
   return
+
+@app.after_request
+def after_request(response):
+   timestamp = strftime('[%Y-%b-%d %H:%M]')
+   LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+   return response
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,7 +3,8 @@ from opentelemetry import trace
 
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
-  def run():
+  def run(logger):
+    logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
@@ -28,24 +29,24 @@ class HomeActivities:
           'created_at': (now - timedelta(days=2)).isoformat()
         }],
       },
-      {
-        'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'Worf',
-        'message': 'I am out of prune juice',
-        'created_at': (now - timedelta(days=7)).isoformat(),
-        'expires_at': (now + timedelta(days=9)).isoformat(),
-        'likes': 0,
-        'replies': []
-      },
-      {
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
-        'created_at': (now - timedelta(hours=1)).isoformat(),
-        'expires_at': (now + timedelta(hours=12)).isoformat(),
-        'likes': 0,
-        'replies': []
-      }
+        {
+          'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+          'handle':  'Worf',
+          'message': 'I am out of prune juice',
+          'created_at': (now - timedelta(days=7)).isoformat(),
+          'expires_at': (now + timedelta(days=9)).isoformat(),
+          'likes': 0,
+          'replies': []
+        },
+        {
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Garek',
+          'message': 'My dear doctor, I am just simple tailor',
+          'created_at': (now - timedelta(hours=1)).isoformat(),
+          'expires_at': (now + timedelta(hours=12)).isoformat(),
+          'likes': 0,
+          'replies': []
+        }
       ]
       span.set_attribute("app.result_length", len(results))
       return results
