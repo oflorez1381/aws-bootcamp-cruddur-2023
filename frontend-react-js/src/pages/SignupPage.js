442f13a461908042c40b83a4e8d90c130354bbe8
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2023-03-08 22:21:50 -0500

    fixing variable

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -26,7 +26,7 @@ export default function SignupPage() {
                 password: password,
                 attributes: {
                     name: name,
-                    email: username,
+                    email: email,
                     preferred_username: username,
                 },
                 autoSignIn: { // optional - enables auto sign in after user is confirmed
