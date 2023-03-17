039c71f6ffc57401f07f3b87efed9ce0311ed1bd
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2023-03-17 17:32:19 -0400

    refactor db library

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,5 +1,8 @@
 import uuid
 from datetime import datetime, timedelta, timezone
+
+# from lib.db import db
+
 class CreateActivity:
   def run(message, user_handle, ttl):
     model = {
@@ -10,19 +13,19 @@ class CreateActivity:
     now = datetime.now(timezone.utc).astimezone()
 
     if (ttl == '30-days'):
-      ttl_offset = timedelta(days=30) 
+      ttl_offset = timedelta(days=30)
     elif (ttl == '7-days'):
-      ttl_offset = timedelta(days=7) 
+      ttl_offset = timedelta(days=7)
     elif (ttl == '3-days'):
-      ttl_offset = timedelta(days=3) 
+      ttl_offset = timedelta(days=3)
     elif (ttl == '1-day'):
-      ttl_offset = timedelta(days=1) 
+      ttl_offset = timedelta(days=1)
     elif (ttl == '12-hours'):
-      ttl_offset = timedelta(hours=12) 
+      ttl_offset = timedelta(hours=12)
     elif (ttl == '3-hours'):
-      ttl_offset = timedelta(hours=3) 
+      ttl_offset = timedelta(hours=3)
     elif (ttl == '1-hour'):
-      ttl_offset = timedelta(hours=1) 
+      ttl_offset = timedelta(hours=1)
     else:
       model['errors'] = ['ttl_blank']
 
@@ -30,16 +33,17 @@ class CreateActivity:
       model['errors'] = ['user_handle_blank']
 
     if message == None or len(message) < 1:
-      model['errors'] = ['message_blank'] 
+      model['errors'] = ['message_blank']
     elif len(message) > 280:
-      model['errors'] = ['message_exceed_max_chars'] 
+      model['errors'] = ['message_exceed_max_chars']
 
     if model['errors']:
       model['data'] = {
         'handle':  user_handle,
         'message': message
-      }   
+      }
     else:
+      self.create_activity()
       model['data'] = {
         'uuid': uuid.uuid4(),
         'display_name': 'Andrew Brown',
@@ -49,3 +53,17 @@ class CreateActivity:
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
+  def create_activity(user_uuid, message, expires_at):
+    sql = f"""
+    INSERT INTO (
+      user_uuid,
+      message,
+      expires_at
+    )
+    VALUES (
+      "{user_uuid}",
+      "{message}",
+      "{expires_at}"
+    )
+    """
+    #query_commit(sql)
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,17 +1,18 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool, query_wrap_array
 
-# tracer = trace.get_tracer("home.activities")
+from lib.db import db
+
+#tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
-  # def run(logger):
   def run(cognito_user_id=None):
-    # logger.info("HomeActivities")
-    # with tracer.start_as_current_span("home-activites-mock-data"):
-    #   span = trace.get_current_span()
-    #   now = datetime.now(timezone.utc).astimezone()
-    #   span.set_attribute("app.now", now.isoformat())
-    sql = query_wrap_array("""
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -27,16 +28,4 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
     """)
-    print("SQL--------------")
-    print(sql)
-    print("SQL--------------")
-    with pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql)
-        # this will return a tuple
-        # the first field being the data
-        json = cur.fetchone()
-    print("-1----")
-    print(json[0])
-    return json[0]
-    #return results
+    return results
