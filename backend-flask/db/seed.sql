47d22e0b86e75f0519b29e124063ac64ea4b2c12
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2023-03-22 20:27:11 -0400

    implement dynamodb into our application

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-    ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-    ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
+    ('Oscar Florez','oscarflorez1381@gmail.com' , 'oflorez' ,'MOCK'),
+    ('Oscar Dario Florez Diaz','oscar_florez1381@hotmail.com' , 'odfd' ,'MOCK');
+    ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
     (
-        (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+        (SELECT uuid from public.users WHERE users.handle = 'oflorez' LIMIT 1),
     'This was imported as seed data!',
                 current_timestamp + interval '10 day'
     )
