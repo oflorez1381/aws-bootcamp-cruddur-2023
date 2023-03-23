e09a614f29c30d884e5e5ba66f9b8739b1df6932
oflorez1381 <oscarflorez1381@gmail.com>
2023-03-23 01:56:40 +0000

    changes in db scripts

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
     ('Oscar Florez','oscarflorez1381@gmail.com' , 'oflorez' ,'MOCK'),
-    ('Oscar Dario Florez Diaz','oscar_florez1381@hotmail.com' , 'odfd' ,'MOCK');
+    ('Oscar Dario Florez Diaz','oscar_florez1381@hotmail.com' , 'odfd' ,'MOCK'),
     ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
