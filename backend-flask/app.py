df8105290b8ef4e711cf1eece525c09ab1c37df0
oflorez1381 <oscarflorez1381@gmail.com>
2023-04-15 00:51:40 +0000

    fixing issues

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -99,7 +99,7 @@ RequestsInstrumentor().instrument()
 
 # Rollbar ----------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
+# @app.before_first_request
 def init_rollbar():
     """init rollbar module"""
     rollbar.init(
