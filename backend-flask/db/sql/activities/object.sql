03d8fb1d7631c2c9569d753a1f8b7b637fd82ec6
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2023-06-08 20:54:00 -0400

    replies

--- backend-flask/db/sql/activities/object.sql
+++ backend-flask/db/sql/activities/object.sql
@@ -1,11 +1,10 @@
-SELECT
-    activities.uuid,
-    users.display_name,
-    users.handle,
-    activities.message,
-    activities.created_at,
-    activities.expires_at
+SELECT activities.uuid,
+       users.display_name,
+       users.handle,
+       activities.message,
+       activities.created_at,
+       activities.expires_at,
+       activities.reply_to_activity_uuid
 FROM public.activities
          INNER JOIN public.users ON users.uuid = activities.user_uuid
-WHERE
-        activities.uuid = %(uuid)s
+WHERE activities.uuid = %(uuid) s
